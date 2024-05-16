b1f2ceb96a534b745f707c362713d1c14ef72efa
Dreamhigh <richardjbrown45@gmail.com>
2024-05-16 07:02:40 -0600

    client

--- config/webtoken.js
+++
@@ -1,9 +0,0 @@
-const jsonwebtoken = require("jsonwebtoken");
-
-const generateJWToken = (id) => {
-  return jsonwebtoken.sign({ id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "10d",
-  });
-};
-
-module.exports = generateJWToken;
